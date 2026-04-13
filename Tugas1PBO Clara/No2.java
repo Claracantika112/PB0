d384f2dd405be1b826c4c97480da0c83870cfbdf
Claracantika112 <cantikaclara131@gmail.com>
2026-04-13 22:08:34 +0700

    Add files via upload

---
+++ Tugas1PBO Clara/No2.java
@@ -0,0 +1,10 @@
+
+    import javax.swing.JOptionPane;
+
+public class No2 {
+    public static void main(String[] args) {
+        String input = JOptionPane.showInputDialog("Anda sedang belajar apa :");
+        JOptionPane.showMessageDialog(null, "BelajarPemrograman Web sangat mudah:"+ input);
+    }
+}
+
